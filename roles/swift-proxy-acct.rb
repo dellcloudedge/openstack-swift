5c1dfb5016e477e97deb935edfc5a529c3033998
aabes <andi_abes@dell.com>
2011-05-23 10:35:02 -0400

    add swift-proxy-acct role which enables account management on the proxy (and change the swift-proxy to not enable it) remove .svn directories some general cleanup

--- roles/swift-proxy-acct.rb
+++ roles/swift-proxy-acct.rb
@@ -1,32 +1,32 @@
 #
-# Cookbook Name:: memcached
-# Definition:: memcached_instance
-#
-# Copyright 2009, Opscode, Inc.
+# Copyright 2011, Dell
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# Author: andi abes
+#
 
-define :memcached_instance do
-  include_recipe "memcached"
+name "swift-proxy-acct"
 
-  runit_service "memcached-#{params[:name]}" do
-    template_name "memcached"
-    cookbook "memcached"
-    options({
-      :memory => node[:memcached][:memory],
-      :port => node[:memcached][:port],
-      :user => node[:memcached][:user]}.merge(params)
-    )
-  end
-end
+description <<AAE
+Provides the proxy and authentication components to swift. 
+This proxy role enables account management, thus it should be deployed with security considerations in mind
+AAE
+
+run_list(
+    "recipe[swift::default]",
+    "recipe[swift::proxy]"
+)
+
+override_attributes "swift" => { "account_management" => "true" }
+
